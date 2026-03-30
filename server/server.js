877366466ee0e2d5ab2775cea1acc9e964277979
Himanshu Kashyap <89869886+Archer1200@users.noreply.github.com>
2026-03-31 03:29:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,14 +8,11 @@ const bookingRouter = require('./routes/bookingRoutes.js')
 
 const app = express()
 
-app.use(cors({ origin: "*" }))
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  next()
-})
-app.options('*', cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json())
 
 
