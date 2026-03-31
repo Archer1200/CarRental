320f7122f58372190a0059d7b6924520f13f2a14
Himanshu Kashyap <89869886+Archer1200@users.noreply.github.com>
2026-03-31 15:49:17 +0530

    Update Hero.jsx

--- CarRental/src/components/Hero.jsx
+++ CarRental/src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
 
   {/* Heading */}
   <h1 className="text-3xl md:text-5xl font-semibold">
-    Luxury cars on Rent--Hello Sumit!!
+    Luxury cars on Rent
   </h1>
 
   {/* Form */}
